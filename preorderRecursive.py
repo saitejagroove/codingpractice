d94ba9e65890c5eec44ddf83593d9806f8bc0e78
saiteja <saiteja.groovey@gmail.com>
2020-10-19 16:02:04 -0400

    19th oct

---
+++ preorderRecursive.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        if root!=None:
+            res.append(root.val)
+            res = res+ self.preorderTraversal(root.left)
+            res = res+ self.preorderTraversal(root.right)
+        else:
+            return []
+        
+        return res
+            
+            
+        
+        
